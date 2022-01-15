33abfd3f41a31151c470033d620338919b1be84e
Whusthat <mueller.sebastian117@gmail.com>
2022-01-15 14:26:00 +0100

    need to build new loop not working

--- day3/part1.py
+++ day3/part1.py
@@ -3,56 +3,21 @@ from chunkString import chunkString
 
 binaryList = read_input("input.txt", 'false')
 
-j = [0, 0]
-k = [0, 0]
-n = [0, 0]
-m = [0, 0]
-o = [0, 0]
-
 mergedList = ''.join(binaryList)
 
 strippedList = list(chunkString(mergedList, 5))
 
-for item in strippedList:
-    for i, v in enumerate(item):
+gammaRate = ""
 
-        if (i == 0) and (v == '1'):
-            j[0] += 1
-        else:
-            j[1] += 1
+for column in strippedList:
+    count = 0
+    for bit in column[0]:
+        bitHolder = int(bit)
 
-        if (i == 1) and (v == '1'):
-            k[0] += 1
-        else:
-            k[1] += 1
+        if bitHolder > 0:
+            count += 1
 
-        if (i == 2) and (v == '1'):
-            n[0] += 1
-        else:
-            n[1] += 1
-
-        if (i == 3) and (v == '1'):
-            m[0] += 1
-        else:
-            m[1] += 1
-
-        if (i == 4) and (v == '1'):
-            o[0] += 1
-        else:
-            o[1] += 1
-
-
-def getBin(x):
-    if x[0] > x[1]:
-        return 1
+    if count > (len(column[0]) - count):
+        gammaRate += "1"
     else:
-        return 0
-
-
-firstBit = getBin(j)
-secondBit = getBin(k)
-thirdBit = getBin(n)
-fourthBit = getBin(m)
-fithBit = getBin(o)
-
-print(fithBit)
+        gammaRate += "0"
