468b7a57e26ce643fc450f009736219d24c4f06f
Whusthat <mueller.sebastian117@gmail.com>
2022-01-15 15:15:11 +0100

    STUCK

--- day3/part1.py
+++ day3/part1.py
@@ -1,23 +1,18 @@
 from readInput import read_input
 from chunkString import chunkString
 
+
 binaryList = read_input("input.txt", 'false')
 
 mergedList = ''.join(binaryList)
 
 strippedList = list(chunkString(mergedList, 5))
 
-gammaRate = ""
+firstBit = ""
 
 for column in strippedList:
-    count = 0
-    for bit in column[0]:
-        bitHolder = int(bit)
-
-        if bitHolder > 0:
-            count += 1
-
-    if count > (len(column[0]) - count):
-        gammaRate += "1"
+    if column[0] == "1":
+        firstBit += "1"
     else:
-        gammaRate += "0"
+        firstBit += "0"
+
---
+++ lib/chunkString.py
@@ -0,0 +1,2 @@
+def chunkString(string, length):
+    return (string[0 + i:length + i] for i in range(0, len(string), length))
