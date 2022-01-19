b6ae7fd9f43851ef726cd3a894d0118152ff8035
Whusthat <mueller.sebastian117@gmail.com>
2022-01-19 14:42:29 +0100

    round function rdy

--- day4/Part1.py
+++ day4/Part1.py
@@ -1,5 +1,6 @@
 with open("input.in") as fin:
     numbers = fin.readline().strip("\n")
+    numbers = numbers.split(",")
     next(fin)
     boardData = [i for i in fin.read().split("\n")]
     boardData = list(filter(None, boardData))
@@ -24,12 +25,18 @@ def bingo_round(number):
     for index, v in enumerate(finalBoards):
         for value in v:
             if value == number:
-                print(finalBoards.index(v), v.index(value))
+                str_index = v.index(value)
+                altered_value = value.replace(value, "HIT")
+                v.pop(str_index)
+                v.insert(str_index, altered_value)
 
 
 finalBoards = remove_whitespace(holder)
 
-bingo_round("12")
+for num in numbers:
+    bingo_round(num)
+
+print(finalBoards)
 
 
 
