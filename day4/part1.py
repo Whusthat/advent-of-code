39c20536c1058a1d0be17dece461d13412c1698d
Whusthat <mueller.sebastian117@gmail.com>
2022-01-30 00:53:59 +0100

    Changed aproach to OOP

--- day4/part1.py
+++ day4/part1.py
@@ -1,18 +1,3 @@
-import math
-
-with open("input.in") as fin:
-    numbers = fin.readline().strip("\n")
-    numbers = numbers.split(",")
-    next(fin)
-    boardData = [i for i in fin.read().split("\n")]
-    boardData = list(filter(None, boardData))
-
-holder = []
-
-for board in boardData:
-    holder.append([board[i:i + 3] for i in range(0, len(board), 3)])
-
-
 def remove_whitespace(liste):
     j = []
     for liste in liste:
@@ -23,36 +8,54 @@ def remove_whitespace(liste):
     return j
 
 
-def bingo_round(number):
-    for index, v in enumerate(finalBoards):
-        for value in v:
-            if value == number:
-                str_index = v.index(value)
-                altered_value = value.replace(value, "HIT")
-                v.pop(str_index)
-                v.insert(str_index, altered_value)
-            if checkWinner():
-                print(math.floor(index / 5))
+with open("test.in") as fin:
+    holder = []
+    holderA = []
+    numbers = fin.readline().strip("\n")
+    numbers = numbers.split(",")
+    next(fin)
+    boardData = [i for i in fin.read().split("\n")]
+    boardData = list(filter(None, boardData))
+    for board in boardData:
+        holder.append([board[i:i + 3] for i in range(0, len(board), 3)])
+    final = remove_whitespace(holder)
+    boards = ([final[i:i + 5] for i in range(0, len(final), 5)])
 
 
-def checkWinner():
-    x = 0
-    y = 0
-    for index, v in enumerate(finalBoards):
-        for number in v:
-            if number == "HIT":
-                x = x + 1
-        if x == 5:
-            return True
-        x = 0
-        y = 0
+class Board:
+    row = 0
+    column = 0
+
+    def __init__(self, list):
+        self.board = list
+
+    def round(self, number):
+        for i, b in enumerate(self.board):
+            for z, c in enumerate(b):
+                if c == number:
+                    b[z] = "hit"
+        self.winner()
+
+    def winner(self):
+        for i, b in enumerate(self.board):
+            for z, c in enumerate(b):
+                if b[z] == "hit":
+                    self.row = self.row + 1
+                if z > 4:
+                    self.row = 0
+                if self.row == 5:
+                    print(self)
+                    return True
 
 
-finalBoards = remove_whitespace(holder)
+objs = [Board(i) for i in boards]
 
-for num in numbers:
-    if bingo_round(num):
-        print("winner")
-        break
 
-print(finalBoards)
+def play(er):
+    for num in er:
+        for obj in objs:
+            if obj.round(num):
+                return obj
+
+
+play(numbers)
