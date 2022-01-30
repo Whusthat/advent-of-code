6adc713b226c99ca6b146c7c809b7a7bd5743869
Whusthat <mueller.sebastian117@gmail.com>
2022-01-30 16:06:04 +0100

    Keep going

--- day4/part1.py
+++ day4/part1.py
@@ -25,27 +25,50 @@ with open("test.in") as fin:
 class Board:
     row = 0
     column = 0
+    winnerBoard = False
 
     def __init__(self, list):
         self.board = list
+
+    def calcb(self, board_to_calc, num):
+        left_numbers = 0
+        for b in board_to_calc:
+            for x in b:
+                if x != "hit":
+                    left_numbers = int(left_numbers + int(x))
+        print(left_numbers * int(num))
 
     def round(self, number):
         for i, b in enumerate(self.board):
             for z, c in enumerate(b):
                 if c == number:
                     b[z] = "hit"
-        self.winner()
+                    self.winner(number)
+                    return True
 
-    def winner(self):
+    def winner(self, num):
         for i, b in enumerate(self.board):
             for z, c in enumerate(b):
                 if b[z] == "hit":
                     self.row = self.row + 1
-                if z > 4:
-                    self.row = 0
                 if self.row == 5:
-                    print(self)
+                    self.calcb(self.board, num)
+                    self.winnerBoard = True
+                    self.row = 0
                     return True
+                if z == 4:
+                    self.row = 0
+                if self.board[z][i] == "hit":
+                    self.column = self.column + 1
+                    print("a")
+                if self.column == 5:
+                    self.calcb(self.board, num)
+                    self.winnerBoard = True
+                    self.column = 0
+                    return True
+                if i == 4:
+                    self.column = 0
+
 
 
 objs = [Board(i) for i in boards]
@@ -54,8 +77,9 @@ objs = [Board(i) for i in boards]
 def play(er):
     for num in er:
         for obj in objs:
-            if obj.round(num):
-                return obj
+            obj.round(num)
+            if obj.winnerBoard:
+                return True
 
 
 play(numbers)
