8652a79e6a8b85bc643463312092af1e7ec36eb6
Whusthat <mueller.sebastian117@gmail.com>
2022-02-03 09:27:22 +0100

    Druing Day 7 Part 2 - Working Solution but Performance issues

--- day7/part2.py
+++ day7/part2.py
@@ -8,35 +8,26 @@ need = 168
 
 def count_fuel(dataset):
     fuel = []
-    positions = [y for y in range(min(dataset), max(dataset) + 1)]
-    i = 0
+    finalFuel = []
+    positionToReach = [x for x in range(max(dataset) + 1)]
 
-    for data in dataset:
-        holder = []
-        for number in positions:
-            fuel_to_destin = []
-            current_pos = data
-            destin_pos = number
-            costs = 0
-            counter = 0
-            if current_pos > destin_pos:
-                for x in range(destin_pos, current_pos):
-                    costs += 1
-                    counter += costs
-                    fuel_to_destin.append(counter)
-            elif current_pos == destin_pos:
-                fuel_to_destin.append(1)
-            else:
-                for x in range(current_pos, destin_pos):
-                    costs += 1
-                    counter += costs
-                    fuel_to_destin.append(counter)
-            holder.append([sum(fuel_to_destin)])
-        fuel.append(holder)
-        i += 1
+    for reach in positionToReach:
+        for i, position in enumerate(dataset):
+            current_fuel = []
+            count_current = 0
+            for step in range(0, max(reach, position) - min(reach, position)):
+                count_current += step + 1
+                if step == (max(reach, position) - min(reach, position) - 1):
+                    current_fuel.append(count_current)
+            fuel.append(sum(current_fuel))
+        finalFuel.append(sum(fuel))
+        fuel.clear()
+
+    return min(finalFuel)
 
 
-    print(fuel)
+
+
 
 
 print("Answer for Part2: ", count_fuel(horizontalPositions))
