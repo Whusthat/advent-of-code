21a4f6b6f66094625ffc30192acb8f3e7be5b1bf
Whusthat <mueller.sebastian117@gmail.com>
2022-02-03 11:33:25 +0100

    Completed Day 7 Part 2 - Cleaned up comments

--- day7/part2-optimized.py
+++ day7/part2-optimized.py
@@ -8,7 +8,7 @@ def count_fuel(dataset):
 
     # calc the increasing fuel costs
     def triangular_cost(steps):
-        return steps * (steps + 1) // 2
+        return steps * (steps + 1) // 2         # steps * (steps + 1) // 2 -> returns an int
 
     # create big number to check for lowest fuel
     minFuel = 2 << 100
@@ -16,10 +16,10 @@ def count_fuel(dataset):
     # loop over every possible position
     for position in range(min(dataset), max(dataset) + 1):
         fuel = 0
+        # check every crab for the current target position
         for crab in dataset:
             fuel += triangular_cost(abs(position - crab))
-
-            # only keep lowest fuel value, so you don not need a stack
+        # only keep lowest fuel value, so you don not need a stack
         minFuel = min(fuel, minFuel)
 
     return minFuel
